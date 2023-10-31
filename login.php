3965f940334579d981b1dd377522ce5e47089c1f
Karina <copto3754@gmail.com>
2023-10-31 14:21:56 -0600

    Se mejoro el inicio de sesion y se verificaron los roles

--- login.php
+++ login.php
@@ -17,40 +17,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["txtcorreo"]; 
     $password = $_POST["txtpassword"]; 
 
-    // Consulta para verificar las credenciales del usuario
-    $query = "SELECT * FROM login WHERE correo='$username' AND password='$password'";
-	$result = $conn->query($query); 
+    // Consulta para obtener la contraseña almacenada en la base de datos
+    $query = "SELECT idusu, password, idrol FROM login WHERE correo='$username'";
+    $result = mysqli_query($conn, $query);
 
     if (mysqli_num_rows($result) == 1) {
-        // Usuario válido
         $row = mysqli_fetch_assoc($result);
+        $stored_password = $row['password'];
         $idrol = $row['idrol'];
 
-        if ($idrol == 1) {
-            // Administrador
-            // Mostrar las solicitudes de préstamo
-            $query = "SELECT * FROM prestamos";
-            $result = mysqli_query($conn, $query);
-            echo '<h3>Lista de Solicitudes de Préstamo</h3>';
-            echo '<ul>';
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo "ID Solicitud: " . $row['idsolicitud'] . "<br>";
-                echo "Nombre: " . $row['nombre'] . "<br>";
-                echo "Monto Solicitado: " . $row['monto'] . "<br>";
-                echo "Plazo: " . $row['plazo'] . " meses<br>";
-                echo "Correo Electrónico: " . $row['correo'] . "<br>";
-                echo "<br>";
+        // Verificar si la contraseña proporcionada coincide con la contraseña almacenada
+        if (password_verify($password, $stored_password)) {
+            // Usuario válido
+            if ($idrol == 1) {
+                // Administrador
+                // Mostrar las solicitudes de préstamo
+                $query = "SELECT * FROM prestamos";
+                $result = mysqli_query($conn, $query);
+                echo '<h3>Lista de Solicitudes de Préstamo</h3>';
+                echo '<ul>';
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "ID Solicitud: " . $row['idsolicitud'] . "<br>";
+                    echo "Nombre: " . $row['nombre'] . "<br>";
+                    echo "Monto Solicitado: " . $row['monto'] . "<br>";
+                    echo "Plazo: " . $row['plazo'] . " meses<br>";
+                    echo "Correo Electrónico: " . $row['correo'] . "<br>";
+                    echo "<br>";
+                }
+                echo '</ul>';
+            } elseif ($idrol == 2) {
+                // Supervisor
+                // Redirigir al usuario a la página de préstamo
+                header("Location: Prestamo.html");
+                exit; // Asegura que el script se detiene
             }
-            echo '</ul>';
-        } elseif ($idrol == 2) {
-            // Supervisor
-            // Redirigir al usuario a la página de préstamo
-            header("Location: Prestamo.html");
-            exit; // Asegura que el script se detiene
+        } else {
+            // Contraseña incorrecta
+            echo "Contraseña incorrecta.";
         }
     } else {
-        // Credenciales inválidas
-        echo "Usuario o contraseña inválidos.";
+        // Usuario no encontrado
+        echo "Usuario no encontrado.";
     }
 }
 
--- registrar.php
+++ registrar.php
@@ -1,33 +1,26 @@
 <?php
-//Para registrar
+// Para registrar
 include('conexion.php');
 
 $correo = mysqli_real_escape_string($conn, $_POST["txtcorreo"]);
 $password = mysqli_real_escape_string($conn, $_POST["txtpassword"]);
 $usu = mysqli_real_escape_string($conn, $_POST["txtnombre"]);
 
+// Hash de la contraseña
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-$queryusuario 	= mysqli_query($conn,"SELECT * FROM login WHERE correo = '$correo'");
-$nr 			= mysqli_num_rows($queryusuario); 
+$queryusuario = mysqli_query($conn, "SELECT * FROM login WHERE correo = '$correo'");
+$nr = mysqli_num_rows($queryusuario);
 
-if ($nr == 0)
-{
-	$queryregistrar = "INSERT INTO login(correo, password, usu) values ('$correo','$password','$usu')";
-	
+if ($nr == 0) {
+    $queryregistrar = "INSERT INTO login(correo, password, usu) values ('$correo','$hashed_password','$usu')";
 
-if(mysqli_query($conn,$queryregistrar))
-{
-	echo "<script> alert('Usuario registrado: $usu');window.location= 'index.html' </script>";
-}
-else 
-{
-	echo "Error: " .$queryregistrar."<br>".mysql_error($conn);
-}
-
-}
-else
-{
-		echo "<script> alert('No puedes registrar este correo: $correo');window.location= 'index.html' </script>";
+    if (mysqli_query($conn, $queryregistrar)) {
+        echo "<script> alert('Usuario registrado: $usu');window.location= 'index.html' </script>";
+    } else {
+        echo "Error: " . $queryregistrar . "<br>" . mysql_error($conn);
+    }
+} else {
+    echo "<script> alert('No puedes registrar este correo: $correo');window.location= 'index.html' </script>";
 }
 ?>
